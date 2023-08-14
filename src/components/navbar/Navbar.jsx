b7cdbfc3a13e093b2402cb68e36890419da4d44b
Steven Perez <stevenalp18@gmail.com>
2023-08-13 20:07:41 -0400

    first commit, let there be Elden Ring

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-neutral-900 p-4 text-white font-['Lora']">
+      <div className="flex align-middle ">
+        <img src="https://i.redd.it/3e2afpjsi4f61.png" width="50" />
+        <h1 className="my-auto text-lg">Elden Ring Directory</h1>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
